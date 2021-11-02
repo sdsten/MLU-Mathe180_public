2f8e95bb406ac45ee36b0a314cacef555f2a993c
savita <sdstenerud@gmail.com>
2021-11-02 22:23:44 +0100

    minor bugfixes

--- OOP/operatorPrecedence0301.java
+++ OOP/operatorPrecedence0301.java
@@ -19,20 +19,25 @@ public class operatorPrecedence0301 {
         // expressions
         System.out.println(i % j - i * k + i);
         System.out.println((((i % j) - (i * k)) + i));
+
         System.out.println(ld % k + kd);
         System.out.println(((ld % k) + kd));
+
         System.out.println(!b && a || b && !a);
         System.out.println((((!b) && a) || (b && (!a))));
+
         System.out.println(j++ - i * k / i);
         j = 3;   // incr. operators have side-effects so this is to reset them
-        System.out.println(((j++) - ((i * k) / i)));
+        System.out.println((((j++) - ((i * k) / i))));
         j = 3;
+
         System.out.println(j - ++i * k / i);
         i = 8;
         System.out.println((j - (((++i) * k) / i)));
-        i = 8;
+
         System.out.println(0 < -k || a == false ^ b == true);
         System.out.println(((0 < (-k)) || ((a == false) ^ (b == true))));
+
         System.out.println(i <= k * j & a);
         System.out.println(((i <= (k * j)) & a));
 
@@ -51,7 +56,7 @@ public class operatorPrecedence0301 {
  *  k = 2;                                                         *
  *  Expressions:                                                   *
  *  1. i % j - i * k + i                                           *
- *  2. ld % k + kd           // hier stand "md" auf dem UeB???     *
+ *  2. ld % k + kd                                                 *
  *  3. !b && a || b && !a                                          *
  *  4. j++ - i * k / i                                             *
  *  5. j - ++i * k / i                                             *
