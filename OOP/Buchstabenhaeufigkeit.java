d2bf6697690c6db07f2a45e1248b4d398ca3b902
savita <sdstenerud@gmail.com>
2021-11-26 22:16:55 +0100

    initial

---
+++ OOP/Buchstabenhaeufigkeit.java
@@ -0,0 +1,71 @@
+// UeB 6, Aufgabe 1, FINISHED, all tests pass
+
+/***********************************************
+ * Note: Sorry, der Code ist etwas messy.      *
+ * Ich hatte diese Woche eine Klausur und      *
+ * leider keine Zeit fuer alles ueber das      *
+ * "make it work (somehow)"-Stadium hinaus :/  *
+ * Ich hoffe auf unserem Anfaenger-Level       *
+ * ist das trotzdem ok.                        *
+ * *********************************************/
+
+public class Buchstabenhaeufigkeit {
+    public static void main(String[] args) {
+
+        // parse string-arguments lowercase only to not have to worry about capital letters
+        String caesarString = args[0].toLowerCase();
+        String commonClear = args[1].toLowerCase(); 
+        
+        int[] occurrences = new int[26];
+        
+        // count occurrences of each character in caesar-string        
+        for (int i = 0; i < caesarString.length(); i++) {
+            
+            char ch = caesarString.charAt(i);
+            
+            // only consider a - z lowercase, ignore everything else
+            if (ch >= 'a' && ch <= 'z') {
+                occurrences[ch - 'a']++;
+            }
+        }
+        
+        // find caesar-equivalent to most common clear char
+        int highestCount = 0;
+        int highestCountIndex = 0;
+        
+        for (int i = 0; i < occurrences.length; i++) {
+             if (occurrences[i] > highestCount) {
+                highestCount = occurrences[i];
+                highestCountIndex = i;
+             }
+        }
+        
+        // print most common caesar char
+        char mostCommonChar = (char)('a'+highestCountIndex);
+        System.out.println(mostCommonChar);
+        
+        // find and print shifts between commonClear and most common caesar char
+        for (int i = 0; i < commonClear.length(); i++) {
+        
+            if (commonClear.charAt(i) < 'a'+highestCountIndex) {
+
+                System.out.println(('a'+highestCountIndex)-commonClear.charAt(i));
+            }
+            else {
+                // mod26 fixes 0-shifts being printed as 26-shifts
+                System.out.println((26 - (commonClear.charAt(i)-('a'+highestCountIndex)))%26);
+            }
+        }
+    }
+}
+
+
+
+/************************************************************
+ * Changelog:                                               *
+ * 21.11.21: skeleton version finished (working)            *
+ *           Issue1: off-by-one error in occurences         *
+ *           Issue2: 0-shift is printed as 26-shift         *
+ * 22.11.21: fixed Issues, finished                         *
+ *           not pretty but good enough                     *
+ * **********************************************************/
---
+++ OOP/Statistik.java
@@ -0,0 +1,81 @@
+// UeB 6, Aufgabe 2, FINISHED, all tests pass, no commas at the end
+
+/***********************************************
+ * Note: Sorry, der Code ist etwas messy.      *
+ * Ich hatte diese Woche eine Klausur und      *
+ * leider keine Zeit fuer alles ueber das      *
+ * "make it work (somehow)"-Stadium hinaus :/  *
+ * Ich hoffe auf unserem Anfaenger-Level       *
+ * ist das trotzdem ok.                        *
+ * *********************************************/
+
+import java.util.Scanner;
+
+public class Statistik {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in); 
+        
+        // get number of diff. ints and total number of inputs
+        int arraySize = Integer.parseInt(args[0]);
+        int numInputs = Integer.parseInt(args[1]); 
+        
+        // create arrays and vars that have to be outside the loop
+        int[] numbers = new int[arraySize];
+        int[] numCount = new int[arraySize];
+        int endOfData = 0;
+        
+        // get input and check it for property new (store) or repeated (incr count)
+        for (int i = 0; i < numInputs; i++) {
+            
+            int num = in.nextInt();
+            int numIndex = 0;
+            boolean isNewNum = true;
+            
+            // check if num is new (note that by not changing isNewNum) or repeating (!isNewNum and get index)
+            for (int j = 0; j < endOfData; j++) {
+                if (num == numbers[j]) {
+                    isNewNum = false;
+                    numIndex = j;
+                    break;
+                }
+            }
+
+            // store new nums and set count to 1, incr count for old nums
+            if (isNewNum) {
+                numbers[endOfData] = num;
+                numCount[endOfData] = 1;
+                endOfData++;
+            }
+            else {
+                numCount[numIndex]++;
+            }
+        }
+
+        // print arrays, without comma at the end
+        for (int i = 0; i < numbers.length && numCount[i] != 0; i++) {
+                System.out.printf(numbers[i] + ((i == (endOfData-1)) ? "" : ", "));   
+        }
+        System.out.println();
+        
+        for (int i = 0; i < numbers.length && numCount[i] != 0; i++) {
+                System.out.printf(numCount[i] + ((i == (endOfData-1)) ? "" : ", "));  
+        }
+        System.out.println();
+        
+    }
+}
+
+
+
+/************************************************************
+ * Changelog:                                               *
+ * 21.11.21: skeleton version finished (working)            *
+ * 22.11.21: removed commas at the end of output            *
+ *           Issue: initializer-0s and data-0s cant be      *
+ *           distinguished, causes bug with ?: at the end   *
+ * 22.11.21: workaround for prev. Issue: manual random      *
+ *           initializer for numbers array                  *
+ * 23.11.21: fixed issue, removed workaround,               *
+ *           not pretty but good enough                     *
+ * **********************************************************/
